9cedccbbf9569893164b78d8f1a4f051d1d98f07
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-08-07 19:21:10 -0400

    forgot to rename these too

--- debug/gcc_all.sh
+++ debug/gcc_all.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-./debug_gcc_cpp11.sh "$@" && \
-./debug_gcc_cpp14.sh "$@" && \
-./debug_gcc_cpp17.sh "$@" && \
-./debug_gcc_cpp20.sh "$@" && \
-./debug_gcc_cpp23.sh "$@"
+#!/bin/bash -x
+SDIR=$(dirname "$0")
+"$SDIR/gcc_cpp11.sh" "$@" && \
+"$SDIR/gcc_cpp14.sh" "$@" && \
+"$SDIR/gcc_cpp17.sh" "$@" && \
+"$SDIR/gcc_cpp20.sh" "$@" && \
+"$SDIR/gcc_cpp23.sh" "$@"
