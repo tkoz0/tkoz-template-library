a6cc0f8b4064bf227aee501e8db7dd8bbc5c60c8
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-08-07 00:28:17 -0400

    more type checks that just make the code only work on gcc probably

--- src/fwnum/util.hpp
+++ src/fwnum/util.hpp
@@ -14,8 +14,56 @@ static_assert(sizeof(int16_t) == 2);
 static_assert(sizeof(int32_t) == 4);
 static_assert(sizeof(int64_t) == 8);
 
+static_assert(std::is_same<uint8_t, unsigned char>::value);
+static_assert(std::is_same<uint16_t, unsigned short>::value);
+static_assert(std::is_same<uint32_t, unsigned int>::value);
+static_assert(std::is_same<uint64_t, unsigned long>::value);
+static_assert(std::is_same<int8_t, signed char>::value);
+static_assert(std::is_same<int16_t, signed short>::value);
+static_assert(std::is_same<int32_t, signed int>::value);
+static_assert(std::is_same<int64_t, signed long>::value);
+
+static_assert(sizeof(void*) == 8);
+static_assert(sizeof(bool*) == 8);
+static_assert(sizeof(float*) == 8);
+static_assert(sizeof(double*) == 8);
+static_assert(sizeof(uint8_t*) == 8);
+static_assert(sizeof(uint16_t*) == 8);
+static_assert(sizeof(uint32_t*) == 8);
+static_assert(sizeof(uint64_t*) == 8);
+static_assert(sizeof(int8_t*) == 8);
+static_assert(sizeof(int16_t*) == 8);
+static_assert(sizeof(int32_t*) == 8);
+static_assert(sizeof(int64_t*) == 8);
+
 namespace tkoz
 {
+
+typedef float fp32_t;
+typedef double fp64_t;
+typedef signed char schar_t;
+typedef unsigned char uchar_t;
+typedef signed short sshort_t;
+typedef unsigned short ushort_t;
+typedef signed int sint_t;
+typedef unsigned int uint_t;
+typedef signed long slong_t;
+typedef unsigned long ulong_t;
+typedef signed long long sll_t;
+typedef unsigned long long ull_t;
+
+static_assert(sizeof(fp32_t) == 4);
+static_assert(sizeof(fp64_t) == 8);
+static_assert(sizeof(schar_t) == 1);
+static_assert(sizeof(uchar_t) == 1);
+static_assert(sizeof(sshort_t) == 2);
+static_assert(sizeof(ushort_t) == 2);
+static_assert(sizeof(sint_t) == 4);
+static_assert(sizeof(uint_t) == 4);
+static_assert(sizeof(slong_t) == 8);
+static_assert(sizeof(ulong_t) == 8);
+static_assert(sizeof(sll_t) == 8);
+static_assert(sizeof(ull_t) == 8);
 
 template <typename T> struct fp_exp_size {};
 template <> struct fp_exp_size<float> { static constexpr size_t value = 8; };
