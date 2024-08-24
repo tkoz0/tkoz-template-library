cd6a375097e7d6acf528df239962b5d73ea3b0e5
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-08-24 08:27:44 -0400

    trying something that does not compile

--- src/meta/math.hpp
+++ src/meta/math.hpp
@@ -9,7 +9,8 @@ struct factorial { static constexpr size_t value = N * factorial<N-1>::value; };
 template <>
 struct factorial<0> { static constexpr size_t value = 1; };
 
-template <size_t N> constexpr size_t factorial_v = factorial<N>::value;
+template <size_t N>
+constexpr size_t factorial_v = factorial<N>::value;
 
 template <size_t A, size_t B>
 struct gcd { static constexpr size_t value = gcd<B,A%B>::value; };
@@ -23,6 +24,29 @@ struct gcd<A,0> { static constexpr size_t value = A; };
 template <size_t B>
 struct gcd<0,B> { static constexpr size_t value = B; };
 
-template <size_t A, size_t B> constexpr size_t gcd_v = gcd<A,B>::value;
+template <size_t A, size_t B>
+constexpr size_t gcd_v = gcd<A,B>::value;
+
+template <size_t N>
+struct fibonacci
+{
+    static constexpr size_t value = fibonacci<N-1>::value + fibonacci<N-1>::_prev;
+private:
+    static constexpr size_t _prev = fibonacci<N-1>::value;
+};
+
+template <>
+struct fibonacci<0> { static constexpr size_t value = 0; };
+
+template <>
+struct fibonacci<1>
+{
+    static constexpr size_t value = 1;
+private:
+    static constexpr size_t _prev = 0;
+};
+
+template <size_t N>
+constexpr size_t fibonacci_v = fibonacci<N>::value;
 
 }
--- test/meta/math.cpp
+++ test/meta/math.cpp
@@ -23,3 +23,13 @@ static_assert(gcd_v<1103,113> == 1);
 static_assert(gcd_v<250,625> == 125);
 static_assert(gcd_v<625,250> == 125);
 
+static_assert(fibonacci_v<0> == 0);
+static_assert(fibonacci_v<1> == 1);
+static_assert(fibonacci_v<2> == 1);
+static_assert(fibonacci_v<3> == 2);
+static_assert(fibonacci_v<4> == 3);
+static_assert(fibonacci_v<5> == 5);
+static_assert(fibonacci_v<6> == 8);
+static_assert(fibonacci_v<7> == 13);
+static_assert(fibonacci_v<8> == 21);
+static_assert(fibonacci_v<9> == 34);
