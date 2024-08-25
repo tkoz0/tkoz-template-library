e7b52f92376241a0a8be4a80f0213704cc346da3
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-08-24 20:49:07 -0400

    metaprogramming

--- src/meta/math.hpp
+++ src/meta/math.hpp
@@ -1,52 +1,177 @@
 #include <cstddef>
+#include <type_traits>
 
 namespace tkoz::meta
 {
 
-template <size_t N>
-struct factorial { static constexpr size_t value = N * factorial<N-1>::value; };
-
-template <>
-struct factorial<0> { static constexpr size_t value = 1; };
-
-template <size_t N>
-constexpr size_t factorial_v = factorial<N>::value;
+// min
 
 template <size_t A, size_t B>
-struct gcd { static constexpr size_t value = gcd<B,A%B>::value; };
+struct min_s
+{
+    static constexpr size_t value = A < B ? A : B;
+};
+
+template <size_t A, size_t B>
+constexpr size_t min_v = min_s<A,B>::value;
+
+// max
+
+template <size_t A, size_t B>
+struct max_s
+{
+    static constexpr size_t value = A > B ? A : B;
+};
+
+template <size_t A, size_t B>
+constexpr size_t max_v = max_s<A,B>::value;
+
+// all
+
+template <bool ...vs>
+struct all_s
+{
+    static constexpr bool value = true;
+};
+
+template <bool v, bool ...vs>
+struct all_s<v,vs...>
+{
+    static constexpr bool value = v && all_s<vs...>::value;
+};
+
+template <bool ...vs>
+constexpr bool all_v = all_s<vs...>::value;
+
+// any
+
+template <bool ...vs>
+struct any_s
+{
+    static constexpr bool value = false;
+};
+
+template <bool v, bool ...vs>
+struct any_s<v,vs...>
+{
+    static constexpr bool value = v || any_s<vs...>::value;
+};
+
+template <bool ...vs>
+constexpr bool any_v = any_s<vs...>::value;
+
+// factorial
+
+template <size_t N>
+struct factorial_s
+{
+    static constexpr size_t value = N * factorial_s<N-1>::value;
+};
+
+template <>
+struct factorial_s<0>
+{
+    static constexpr size_t value = 1;
+};
+
+template <size_t N>
+constexpr size_t factorial_v = factorial_s<N>::value;
+
+// fibonacci
+
+template <size_t N>
+struct fibonacci_s
+{
+    static constexpr size_t value = fibonacci_s<N-1>::value + fibonacci_s<N-1>::_prev;
+    static constexpr size_t _prev = fibonacci_s<N-1>::value;
+};
+
+template <>
+struct fibonacci_s<0>
+{
+    static constexpr size_t value = 0;
+};
+
+template <>
+struct fibonacci_s<1>
+{
+    static constexpr size_t value = 1;
+    static constexpr size_t _prev = 0;
+};
+
+template <size_t N>
+constexpr size_t fibonacci_v = fibonacci_s<N>::value;
+
+// gcd
+
+template <size_t A, size_t B>
+struct gcd
+{
+    static constexpr size_t value = gcd<B,A%B>::value;
+};
 
 template <>
 struct gcd<0,0> {};
 
 template <size_t A>
-struct gcd<A,0> { static constexpr size_t value = A; };
-
-template <size_t B>
-struct gcd<0,B> { static constexpr size_t value = B; };
+struct gcd<A,0>
+{
+    static constexpr size_t value = A;
+};
 
 template <size_t A, size_t B>
 constexpr size_t gcd_v = gcd<A,B>::value;
 
-template <size_t N>
-struct fibonacci
+namespace _internal
 {
-    static constexpr size_t value = fibonacci<N-1>::value + fibonacci<N-1>::_prev;
-private:
-    static constexpr size_t _prev = fibonacci<N-1>::value;
+
+template <size_t i, size_t j>
+struct _permutations_mulseq
+{
+    static_assert(i > j);
+    static constexpr size_t _value = i * _permutations_mulseq<i-1,j>::_value;
 };
 
-template <>
-struct fibonacci<0> { static constexpr size_t value = 0; };
-
-template <>
-struct fibonacci<1>
+template <size_t i>
+struct _permutations_mulseq<i,i>
 {
-    static constexpr size_t value = 1;
-private:
-    static constexpr size_t _prev = 0;
+    static constexpr size_t _value = 1;
 };
 
-template <size_t N>
-constexpr size_t fibonacci_v = fibonacci<N>::value;
+template <size_t n, size_t k, size_t i = k>
+struct _combinations_mulseq
+{
+    static constexpr size_t _value = _combinations_mulseq<n,k,k-1>::_value * (n-k+1) / k;
+};
+
+template <size_t n, size_t k>
+struct _combinations_mulseq<n,k,0>
+{
+    static constexpr size_t _value = 1;
+};
+
+}
+
+// permutations
+
+template <size_t N, size_t K>
+struct permutations_s
+{
+    static constexpr size_t value = K > N ? 0 : _internal::_permutations_mulseq<N,N-K>::_value;
+};
+
+template <size_t N, size_t K>
+constexpr size_t permutations_v = permutations_s<N,K>::value;
+
+// combinations
+
+template <size_t N, size_t K>
+struct combinations_s
+{
+    static constexpr size_t value = K <= N/2 ? _internal::_combinations_mulseq<N,K>::_value : combinations_s<N,N-K>::value;
+};
+
+template <size_t N, size_t K>
+constexpr size_t combinations_v = combinations_s<N,K>::value;
 
 }
--- test/meta/math.cpp
+++ test/meta/math.cpp
@@ -1,35 +1,124 @@
 #include "../../src/meta/math.hpp"
-using namespace tkoz::meta;
+#include <array>
+namespace meta = tkoz::meta;
 int main() {}
+#define ARRLEN(arr) (sizeof(arr)/sizeof(arr[0]))
 
-static_assert(factorial_v<0> == 1);
-static_assert(factorial_v<1> == 1);
-static_assert(factorial_v<2> == 2);
-static_assert(factorial_v<3> == 6);
-static_assert(factorial_v<4> == 24);
-static_assert(factorial_v<5> == 120);
-static_assert(factorial_v<6> == 720);
-static_assert(factorial_v<7> == 5040);
-static_assert(factorial_v<8> == 40320);
-static_assert(factorial_v<9> == 362880);
+// min
 
-static_assert(gcd_v<1,0> == 1);
-static_assert(gcd_v<2,4> == 2);
-static_assert(gcd_v<6,15> == 3);
-static_assert(gcd_v<20,52> == 4);
-static_assert(gcd_v<24,0> == 24);
-static_assert(gcd_v<113,1103> == 1);
-static_assert(gcd_v<1103,113> == 1);
-static_assert(gcd_v<250,625> == 125);
-static_assert(gcd_v<625,250> == 125);
+static_assert(meta::min_v<0,0> == 0);
+static_assert(meta::min_v<0,1> == 0);
+static_assert(meta::min_v<1,0> == 0);
+static_assert(meta::min_v<12,11> == 11);
+static_assert(meta::min_v<11,12> == 11);
+static_assert(meta::min_v<12,12> == 12);
+static_assert(meta::min_v<12,13> == 12);
+static_assert(meta::min_v<13,12> == 12);
 
-static_assert(fibonacci_v<0> == 0);
-static_assert(fibonacci_v<1> == 1);
-static_assert(fibonacci_v<2> == 1);
-static_assert(fibonacci_v<3> == 2);
-static_assert(fibonacci_v<4> == 3);
-static_assert(fibonacci_v<5> == 5);
-static_assert(fibonacci_v<6> == 8);
-static_assert(fibonacci_v<7> == 13);
-static_assert(fibonacci_v<8> == 21);
-static_assert(fibonacci_v<9> == 34);
+// max
+
+static_assert(meta::max_v<0,0> == 0);
+static_assert(meta::max_v<0,1> == 1);
+static_assert(meta::max_v<1,0> == 1);
+static_assert(meta::max_v<12,11> == 12);
+static_assert(meta::max_v<11,12> == 12);
+static_assert(meta::max_v<12,12> == 12);
+static_assert(meta::max_v<12,13> == 13);
+static_assert(meta::max_v<13,12> == 13);
+
+// all
+
+static_assert(meta::all_v<>);
+static_assert(meta::all_v<true>);
+static_assert(!meta::all_v<false>);
+static_assert(!meta::all_v<false,false>);
+static_assert(!meta::all_v<false,true>);
+static_assert(!meta::all_v<true,false>);
+static_assert(meta::all_v<true,true>);
+static_assert(!meta::all_v<true,true,true,true,true,false>);
+static_assert(meta::all_v<true,true,true,true>);
+static_assert(!meta::all_v<false,true,true,true>);
+
+// any
+
+static_assert(!meta::any_v<>);
+static_assert(meta::any_v<true>);
+static_assert(!meta::any_v<false>);
+static_assert(!meta::any_v<false,false>);
+static_assert(meta::any_v<true,false>);
+static_assert(meta::any_v<false,true>);
+static_assert(meta::any_v<true,true>);
+static_assert(!meta::any_v<false,false,false,false,false>);
+static_assert(meta::any_v<true,false,false,false>);
+static_assert(meta::any_v<false,false,false,true>);
+
+// factorial
+
+constexpr size_t tests_factorial[] =
+{
+    1ull,1ull,2ull,6ull,24ull,120ull,720ull,5040ull,40320ull,362880ull,3628800ull,
+    39916800ull,479001600ull,6227020800ull,87178291200ull,1307674368000ull,
+    20922789888000ull,355687428096000ull,6402373705728000ull,121645100408832000ull,2432902008176640000ull
+};
+
+template <size_t N = 0>
+constexpr bool run_factorial() { return meta::factorial_v<N> == tests_factorial[N] && run_factorial<N+1>(); }
+
+template <>
+constexpr bool run_factorial<ARRLEN(tests_factorial)>() { return true; }
+
+static_assert(run_factorial());
+
+// fibonacci
+
+constexpr size_t tests_fibonacci[] =
+{
+    0ull,1ull,1ull,2ull,3ull,5ull,8ull,13ull,21ull,34ull,55ull,89ull,144ull,233ull,377ull,610ull,987ull,1597ull,2584ull,4181ull,6765ull,
+    10946ull,17711ull,28657ull,46368ull,75025ull,121393ull,196418ull,317811ull,514229ull,832040ull,
+    1346269ull,2178309ull,3524578ull,5702887ull,9227465ull,14930352ull,24157817ull,39088169ull,63245986ull,102334155ull,
+    165580141ull,267914296ull,433494437ull,701408733ull,1134903170ull,
+    1836311903ull,2971215073ull,4807526976ull,7778742049ull,12586269025ull,
+    20365011074ull,32951280099ull,53316291173ull,86267571272ull,139583862445ull,
+    225851433717ull,365435296162ull,591286729879ull,956722026041ull,1548008755920ull,
+    2504730781961ull,4052739537881ull,6557470319842ull,10610209857723ull,17167680177565ull,
+    27777890035288ull,44945570212853ull,72723460248141ull,117669030460994ull,190392490709135ull,
+    308061521170129ull,498454011879264ull,806515533049393ull,1304969544928657ull,2111485077978050ull,
+    3416454622906707ull,5527939700884757ull,8944394323791464ull,14472334024676221ull,23416728348467685ull,
+    37889062373143906ull,61305790721611591ull,99194853094755497ull,160500643816367088ull,259695496911122585ull,
+    420196140727489673ull,679891637638612258ull,1100087778366101931ull,1779979416004714189ull,2880067194370816120ull,
+    4660046610375530309ull,7540113804746346429ull,12200160415121876738ull
+};
+
+template <size_t N = 0>
+constexpr bool run_fibonacci() { return meta::fibonacci_v<N> == tests_fibonacci[N] && run_fibonacci<N+1>(); }
+
+template <>
+constexpr bool run_fibonacci<ARRLEN(tests_fibonacci)>() { return true; }
+
+static_assert(run_fibonacci());
+
+// gcd
+
+static_assert(meta::gcd_v<1,0> == 1);
+static_assert(meta::gcd_v<0,1> == 1);
+static_assert(meta::gcd_v<2,4> == 2);
+static_assert(meta::gcd_v<2,5> == 1);
+static_assert(meta::gcd_v<2,6> == 2);
+static_assert(meta::gcd_v<4,2> == 2);
+static_assert(meta::gcd_v<5,2> == 1);
+static_assert(meta::gcd_v<6,2> == 2);
+static_assert(meta::gcd_v<18,15> == 3);
+static_assert(meta::gcd_v<18,12> == 6);
+static_assert(meta::gcd_v<75,15> == 15);
+static_assert(meta::gcd_v<1103,113> == 1);
+static_assert(meta::gcd_v<113,1103> == 1);
+static_assert(meta::gcd_v<516,3648> == 12);
+static_assert(meta::gcd_v<3648,516> == 12);
+
+// permutations
+
+
+
+// combinations
+
+
