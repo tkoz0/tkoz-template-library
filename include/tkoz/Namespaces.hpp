4f156b26ffc6f869ee297984e74ddd1a1826aef9
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-05-26 00:29:18 -0400

    namespace overview/summary

---
+++ include/tkoz/Namespaces.hpp
@@ -0,0 +1,22 @@
+/// \mainpage TKoz Template Library
+///
+/// This is a learning experiment to study software/interface design, advanced
+/// C++ features, and various related goals.
+///
+
+/// \brief personal C++ libraries
+namespace tkoz
+{
+    /// \brief custom library similar to standard template library
+    namespace stl
+    {
+        /// \brief template metaprogramming tools
+        namespace meta {}
+    }
+
+    /// \brief contains strings for coloring and formatting terminal output
+    namespace cli {}
+
+    /// \brief contains utilities for compile time calculations
+    namespace metaCalc {}
+}
