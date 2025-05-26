0fa6475f795cb01232732bfe908260ebcce70ce8
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-05-26 00:22:51 -0400

    exit codes

---
+++ include/tkoz/cli/Exit.hpp
@@ -0,0 +1,15 @@
+///
+/// Return codes for program exit
+///
+
+namespace tkoz::cli
+{
+
+/// \brief typical return codes for basic programs
+enum ReturnCode: int
+{
+    cExitSuccess = 0,
+    cExitFailure = 1
+};
+
+} // namespace tkoz::cli
