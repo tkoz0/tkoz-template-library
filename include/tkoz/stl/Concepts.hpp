1e6d9f2aa78efda151ce33c4351951392bdee339
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2025-05-30 17:43:21 -0400

    concepts for basic operators

--- include/tkoz/stl/Concepts.hpp
+++ include/tkoz/stl/Concepts.hpp
@@ -7,8 +7,18 @@
 #include <tkoz/stl/Meta.hpp>
 #include <tkoz/stl/Types.hpp>
 
+#include <compare>
+
 namespace tkoz::stl::concepts
 {
+
+/// 2 types are the same
+template <typename T, typename U>
+concept isSame = meta::isSame<T,U> && meta::isSame<U,T>;
+
+/// type T is the same as any of Us...
+template <typename T, typename ...Us>
+concept isSameAsAny = (false || ... || isSame<T,Us>);
 
 /// type is void
 template <typename T>
@@ -27,10 +37,27 @@ concept isPrimitiveFloatingPoint =
 template <typename T>
 concept isAnyChar = meta::isSameAsAny<T,char,unsigned char,signed char>;
 
+// char is signed even though char is different type from signed char
+static_assert(static_cast<char>(-1) < static_cast<char>(0));
+
+// checks for signed/unsigned integers
+static_assert(static_cast<signed char>(-1) < static_cast<signed char>(0));
+static_assert(static_cast<unsigned char>(-1) > static_cast<unsigned char>(0));
+static_assert(static_cast<signed short>(-1) < static_cast<signed short>(0));
+static_assert(static_cast<unsigned short>(-1) > static_cast<unsigned short>(0));
+static_assert(static_cast<signed int>(-1) < static_cast<signed int>(0));
+static_assert(static_cast<unsigned int>(-1) > static_cast<unsigned int>(0));
+static_assert(static_cast<signed long>(-1) < static_cast<signed long>(0));
+static_assert(static_cast<unsigned long>(-1) > static_cast<unsigned long>(0));
+static_assert(static_cast<signed long long>(-1)
+    < static_cast<signed long long>(0));
+static_assert(static_cast<unsigned long long>(-1)
+    > static_cast<unsigned long long>(0));
+
 /// type is a primitive signed integer
 template <typename T>
 concept isPrimitiveSignedInteger =
-    meta::isSameAsAny<T,signed char,signed short,signed int,
+    meta::isSameAsAny<T,char,signed char,signed short,signed int,
                         signed long,signed long long>;
 
 /// type is a primitive unsigned integer
@@ -47,5 +74,367 @@ concept isPrimitiveInteger = isPrimitiveSignedInteger<T>
 /// type is primitive integer or bool (integral)
 template <typename T>
 concept isPrimitiveIntegerOrBool = isPrimitiveInteger<T> || isBool<T>;
+
+/// type is primitive number (integer or floating point)
+template <typename T>
+concept isPrimitiveNumber = isPrimitiveInteger<T>
+    || isPrimitiveFloatingPoint<T>;
+
+/// type is primitive
+template <typename T>
+concept isPrimitive = isPrimitiveIntegerOrBool<T>
+    || isPrimitiveFloatingPoint<T>;
+
+//
+// Checks on validity of calling operators and copy constructor
+// These have expected result types that are applicable to common types
+//
+
+/// Can a SrcType object be used to construct a DstType object
+/// \note If DstType and SrcType are the same, this checks the copy constructor
+template <typename DstType, typename SrcType>
+concept canConvert = requires (SrcType value)
+{ { meta::RemoveRef<DstType>(value) } -> isSame<meta::RemoveRef<DstType>>; };
+
+/// Can a SrcType object be statically casted to a DstType object
+template <typename DstType, typename SrcType>
+concept canStaticCast = requires (SrcType value)
+{ { static_cast<DstType>(value) } -> isSame<DstType>; };
+
+/// Can a SrcType object be dynamically casted to a DstType object
+template <typename DstType, typename SrcType>
+concept canDynamicCast = requires (SrcType value)
+{ { dynamic_cast<DstType>(value) } -> isSame<DstType>; };
+
+/// Can a SrcType object be const casted to a DstType object
+template <typename DstType, typename SrcType>
+concept canConstCast = requires (SrcType value)
+{ { const_cast<DstType>(value) } -> isSame<DstType>; };
+
+/// Can a SrcType object be reinterpret casted to a DstType object
+template <typename DstType, typename SrcType>
+concept canReinterpretCast = requires (SrcType value)
+{ { reinterpret_cast<DstType>(value) } -> isSame<DstType>; };
+
+/// Can a SrcType object be assigned to a DstType object (a = b)
+/// And is the result a DstType reference as usual
+template <typename DstType, typename SrcType>
+concept canAssign = requires (DstType dst, SrcType src)
+{ { dst = src } -> isSame<meta::AddLVRef<DstType>>; };
+
+/// Can Type be added to same type
+template <typename Type>
+concept canAddSame = requires (Type a, Type b)
+{ { a + b } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can Type be subtracted to same type
+template <typename Type>
+concept canSubSame = requires (Type a, Type b)
+{ { a - b } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Does Type support unary plus to same type
+template <typename Type>
+concept canPosSame = requires (Type t)
+{ { +t } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Does Type support unary minus to same type
+template <typename Type>
+concept canNegSame = requires (Type t)
+{ { -t } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can Type be multiplied to same type
+template <typename Type>
+concept canMulSame = requires (Type a, Type b)
+{ { a * b } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can Type be divided to same type
+template <typename Type>
+concept canDivSame = requires (Type a, Type b)
+{ { a / b } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Does Type support modulus to same type
+template <typename Type>
+concept canModSame = requires (Type a, Type b)
+{ { a % b } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can a Type object be pre incremented (with a Type& result)
+template <typename Type>
+concept canPreInc = requires (Type t)
+{ { ++t } -> isSame<meta::AddLVRef<Type>>; };
+
+/// Can a Type object be post incremented (with a Type result)
+template <typename Type>
+concept canPostInc = requires (Type t)
+{ { t++ } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can a Type object be pre decremented (with a Type& result)
+template <typename Type>
+concept canPreDec = requires (Type t)
+{ { --t } -> isSame<meta::AddLVRef<Type>>; };
+
+/// Can a Type object be post decremented (with a Type result)
+template <typename Type>
+concept canPostDec = requires (Type t)
+{ { t-- } -> isSame<meta::RemoveRef<Type>>; };
+
+/// Can 2 types be compared equal (bool)
+template <typename LeftType, typename RightType>
+concept canCmpEq = requires (LeftType l, RightType r)
+{ { l == r } -> isSame<bool>; };
+
+/// Can 2 types be compared not equal (bool)
+template <typename LeftType, typename RightType>
+concept canCmpNe = requires (LeftType l, RightType r)
+{ { l != r } -> isSame<bool>; };
+
+/// Can 2 types be compared less than (bool)
+template <typename LeftType, typename RightType>
+concept canCmpLt = requires (LeftType l, RightType r)
+{ { l < r } -> isSame<bool>; };
+
+/// Can 2 types be compared less than or equal to (bool)
+template <typename LeftType, typename RightType>
+concept canCmpLe = requires (LeftType l, RightType r)
+{ { l <= r } -> isSame<bool>; };
+
+/// Can 2 types be compared greater than (bool)
+template <typename LeftType, typename RightType>
+concept canCmpGt = requires (LeftType l, RightType r)
+{ { l > r } -> isSame<bool>; };
+
+/// Can 2 types be compared greater than or equal to (bool)
+template <typename LeftType, typename RightType>
+concept canCmpGe = requires (LeftType l, RightType r)
+{ { l >= r } -> isSame<bool>; };
+
+/// Can 2 types be compared 3 way (std::strong_ordering)
+template <typename LeftType, typename RightType>
+concept canCmp3way = requires (LeftType l, RightType r)
+{ { l <=> r } -> isSame<std::strong_ordering>; };
+
+/// Is the address of a type the expected pointer type
+template <typename Type>
+concept canAddr = requires (Type t)
+{ { &t } -> isSame<meta::RemoveRef<Type>*>; };
+
+/// Can 2 types be compound added
+template <typename LeftType, typename RightType>
+concept canAddEq = requires (LeftType l, RightType r)
+{ { l += r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types be compound subtracted
+template <typename LeftType, typename RightType>
+concept canSubEq = requires (LeftType l, RightType r)
+{ { l -= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types be compound multiplied
+template <typename LeftType, typename RightType>
+concept canMulEq = requires (LeftType l, RightType r)
+{ { l *= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types be compound divided
+template <typename LeftType, typename RightType>
+concept canDivEq = requires (LeftType l, RightType r)
+{ { l /= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound modulo
+template <typename LeftType, typename RightType>
+concept canModEq = requires (LeftType l, RightType r)
+{ { l %= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound and
+template <typename LeftType, typename RightType>
+concept canAndEq = requires (LeftType l, RightType r)
+{ { l &= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound or
+template <typename LeftType, typename RightType>
+concept canOrEq = requires (LeftType l, RightType r)
+{ { l |= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound xor
+template <typename LeftType, typename RightType>
+concept canXorEq = requires (LeftType l, RightType r)
+{ { l ^= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound shift left
+template <typename LeftType, typename RightType>
+concept canShiftLeftEq = requires (LeftType l, RightType r)
+{ { l <<= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+/// Can 2 types support compound shift right
+template <typename LeftType, typename RightType>
+concept canShiftRightEq = requires (LeftType l, RightType r)
+{ { l >>= r } -> isSame<meta::AddLVRef<LeftType>>; };
+
+//
+// Check if an operator is supported with some types
+// The result type is ignored
+//
+
+/// Do 2 types have addition (a + b)
+template <typename LeftType, typename RightType>
+concept hasAdd = requires (LeftType l, RightType r) { l + r; };
+
+/// Do 2 types have subtraction (a - b)
+template <typename LeftType, typename RightType>
+concept hasSub = requires (LeftType l, RightType r) { l - r; };
+
+/// Does a type have unary + (+obj)
+template <typename Type>
+concept hasPos = requires (Type t) { +t; };
+
+/// Does a type have unary - (-obj)
+template <typename Type>
+concept hasNeg = requires (Type t) { -t; };
+
+/// Do 2 types have multiplication (a * b)
+template <typename LeftType, typename RightType>
+concept hasMul = requires (LeftType l, RightType r) { l * r; };
+
+/// Do 2 types have division (a / b)
+template <typename LeftType, typename RightType>
+concept hasDiv = requires (LeftType l, RightType r) { l / r; };
+
+/// Do 2 types have modulo
+template <typename LeftType, typename RightType>
+concept hasMod = requires (LeftType l, RightType r) { l % r; };
+
+/// Does a type have prefix increment (++obj)
+template <typename Type>
+concept hasPreInc = requires (Type t) { ++t; };
+
+/// Does a type have postfix increment (obj++)
+template <typename Type>
+concept hasPostInc = requires (Type t) { t++; };
+
+/// Does a type have prefix decrement (--obj)
+template <typename Type>
+concept hasPreDec = requires (Type t) { --t; };
+
+/// Does a type have postfix decrement (obj--)
+template <typename Type>
+concept hasPostDec = requires (Type t) { t--; };
+
+/// Do 2 types have compare equal
+template <typename LeftType, typename RightType>
+concept hasCmpEq = requires (LeftType l, RightType r) { l == r; };
+
+/// Do 2 types have compare not equal
+template <typename LeftType, typename RightType>
+concept hasCmpNe = requires (LeftType l, RightType r) { l != r; };
+
+/// Do 2 types have compare less than
+template <typename LeftType, typename RightType>
+concept hasCmpLt = requires (LeftType l, RightType r) { l < r; };
+
+/// Do 2 types have compare less than or equal to
+template <typename LeftType, typename RightType>
+concept hasCmpLe = requires (LeftType l, RightType r) { l <= r; };
+
+/// Do 2 types have compare greater than
+template <typename LeftType, typename RightType>
+concept hasCmpGt = requires (LeftType l, RightType r) { l > r; };
+
+/// Do 2 types have compare greater than or equal to
+template <typename LeftType, typename RightType>
+concept hasCmpGe = requires (LeftType l, RightType r) { l >= r; };
+
+/// Does a type have logical not
+template <typename Type>
+concept hasLNot = requires (Type t) { !t; };
+
+/// Do 2 types have logical and
+template <typename LeftType, typename RightType>
+concept hasLAnd = requires (LeftType l, RightType r) { l && r; };
+
+/// Do 2 types have logical or
+template <typename LeftType, typename RightType>
+concept hasLOr = requires (LeftType l, RightType r) { l || r; };
+
+/// Does a type have bit not
+template <typename Type>
+concept hasBitNot = requires (Type t) { ~t; };
+
+/// Do 2 types have bit and
+template <typename LeftType, typename RightType>
+concept hasBitAnd = requires (LeftType l, RightType r) { l & r; };
+
+/// Do 2 types have bit or
+template <typename LeftType, typename RightType>
+concept hasBitOr = requires (LeftType l, RightType r) { l | r; };
+
+/// Do 2 types have bit xor
+template <typename LeftType, typename RightType>
+concept hasBitXor = requires (LeftType l, RightType r) { l ^ r; };
+
+/// Do 2 types have shift left
+template <typename LeftType, typename RightType>
+concept hasShiftLeft = requires (LeftType l, RightType r) { l << r; };
+
+/// Do 2 types have shift right
+template <typename LeftType, typename RightType>
+concept hasShiftRight = requires (LeftType l, RightType r) { l >> r; };
+
+/// Do 2 types have assignment
+template <typename LeftType, typename RightType>
+concept hasAssign = requires (LeftType l, RightType r) { l = r; };
+
+/// Do 2 types have compound add
+template <typename LeftType, typename RightType>
+concept hasAddEq = requires (LeftType l, RightType r) { l += r; };
+
+/// Do 2 types have compound subtract
+template <typename LeftType, typename RightType>
+concept hasSubEq = requires (LeftType l, RightType r) { l -= r; };
+
+/// Do 2 types have compound multiply
+template <typename LeftType, typename RightType>
+concept hasMulEq = requires (LeftType l, RightType r) { l *= r; };
+
+/// Do 2 types have compound divide
+template <typename LeftType, typename RightType>
+concept hasDivEq = requires (LeftType l, RightType r) { l /= r; };
+
+/// Do 2 types have compound modulus
+template <typename LeftType, typename RightType>
+concept hasModEq = requires (LeftType l, RightType r) { l %= r; };
+
+/// Do 2 types have compound and
+template <typename LeftType, typename RightType>
+concept hasAndEq = requires (LeftType l, RightType r) { l &= r; };
+
+/// Do 2 types have compound or
+template <typename LeftType, typename RightType>
+concept hasOrEq = requires (LeftType l, RightType r) { l |= r; };
+
+/// Do 2 types have compound xor
+template <typename LeftType, typename RightType>
+concept hasXorEq = requires (LeftType l, RightType r) { l ^= r; };
+
+/// Do 2 types have compound shift left
+template <typename LeftType, typename RightType>
+concept hasShiftLeftEq = requires (LeftType l, RightType r) { l <<= r; };
+
+/// Do 2 types have compound shift right
+template <typename LeftType, typename RightType>
+concept hasShiftRightEq = requires (LeftType l, RightType r) { l >>= r; };
+
+/// Do a type have subscript with given args
+template <typename LeftType, typename ...ArgTypes>
+concept hasSubscript = requires (LeftType l, ArgTypes ...a) { l[a...]; };
+// TODO visual studio code error with expanding parameter pack in subscript
+
+/// Does a type have dereference
+template <typename Type>
+concept hasDeref = requires (Type t) { *t; };
+
+/// Does a type have address of
+template <typename Type>
+concept hasAddr = requires (Type t) { &t; };
+
+/// Does a type have calling with given args
+template <typename LeftType, typename ...ArgTypes>
+concept hasCall = requires (LeftType l, ArgTypes ...a) { l(a...); };
 
 } // namespace tkoz::stl::concepts
--- test/tkoz/stl/Concepts.cpp
+++ test/tkoz/stl/Concepts.cpp
@@ -3,11 +3,77 @@
 //
 
 #include <tkoz/stl/Concepts.hpp>
+#include <tkoz/stl/Meta.hpp>
+
+#include <string>
+
+// the following template/macro stuff is for checking a concept applies
+// (or does not apply) to a variable list of types
+// these check some concepts in tkoz::stl::concepts
+// concepts cannot be passed as template parameters in C++ for now
+
+// check parameter pack is all true
+template <bool ...values>
+static constexpr bool packAllTrue = (true && ... && values);
+static_assert(packAllTrue<true,true,true,true>);
+static_assert(!packAllTrue<true,true,false,true>);
+
+// check parameter pack is all false
+template <bool ...values>
+static constexpr bool packAllFalse = !(false || ... || values);
+static_assert(packAllFalse<false,false,false,false>);
+static_assert(!packAllFalse<false,true,false,false>);
+
+// check for a concept in tkoz::stl::concepts
+// requires it is true for all types in __VA_ARGS__
+#define CHECK_ALL_TRUE(cppConcept,...) \
+template <typename ...Ts> \
+concept _check_all_true_##cppConcept = requires (Ts ...ts) \
+{ typename tkoz::stl::meta::EnableIf< \
+    packAllTrue<tkoz::stl::concepts::cppConcept<decltype(ts)>...>,void>; }; \
+static_assert(_check_all_true_##cppConcept<__VA_ARGS__>)
+
+// check for a concept in tkoz::stl::concepts
+// requires it is false for all types in __VA_ARGS__
+#define CHECK_ALL_FALSE(cppConcept,...) \
+template <typename ...Ts> \
+concept _check_all_false_##cppConcept = requires (Ts ...ts) \
+{ typename tkoz::stl::meta::EnableIf< \
+    packAllFalse<tkoz::stl::concepts::cppConcept<decltype(ts)>...>,void>; }; \
+static_assert(_check_all_false_##cppConcept<__VA_ARGS__>)
+
+// all numbers at least 4 bytes should support arithmetic with itself
+// such that the result is the same type, no implicit conversion occurs
+// operations involving char/short are implicitly converted up to int
+// if the types sizes differ, result is promoted to the larger type
+// if unsigned is involved with same size types
+//   then result is implicitly "promoted" to unsigned
+#define INTS(sign) sign int, sign long, sign long long
+#define SINTS INTS(signed)
+#define UINTS INTS(unsigned)
+#define ALL_INTS SINTS, UINTS
+#define FLOATS float, double, long double
+#define ALL_NUMS ALL_INTS, FLOATS
+#define ALL_PRIMS bool, char, ALL_NUMS
+
+CHECK_ALL_TRUE(canAddSame,ALL_NUMS);
+CHECK_ALL_TRUE(canSubSame,ALL_NUMS);
+CHECK_ALL_TRUE(canPosSame,ALL_NUMS);
+CHECK_ALL_TRUE(canNegSame,ALL_NUMS);
+CHECK_ALL_TRUE(canMulSame,ALL_NUMS);
+CHECK_ALL_TRUE(canDivSame,ALL_NUMS);
+CHECK_ALL_TRUE(canModSame,ALL_INTS); // mod not supported for floats
+CHECK_ALL_FALSE(canModSame,FLOATS);
+CHECK_ALL_TRUE(canPreInc,ALL_NUMS); // inc/dec does work on floats
+CHECK_ALL_TRUE(canPostInc,ALL_NUMS);
+CHECK_ALL_TRUE(canPreDec,ALL_NUMS);
+CHECK_ALL_TRUE(canPostDec,ALL_NUMS);
 
 // only compile time tests needed, not running anything
 int main()
 {
     using namespace tkoz::stl::concepts;
+    namespace meta = tkoz::stl::meta;
 
     // isVoid
     static_assert(isVoid<void>);
@@ -48,4 +114,58 @@ int main()
     static_assert(isPrimitiveUnsignedInteger<unsigned long long>);
     static_assert(!isPrimitiveUnsignedInteger<int>);
     static_assert(!isPrimitiveUnsignedInteger<bool>);
+
+    // canConvert
+    static_assert(canConvert<long,long>);
+    static_assert(canConvert<int,bool>);
+    static_assert(canConvert<int&&,float&&>);
+    static_assert(canConvert<double&,float>);
+    static_assert(canConvert<double,float>);
+    static_assert(canConvert<float,bool>);
+    static_assert(!canConvert<bool,std::string>);
+    static_assert(!canConvert<std::string,float>);
+
+    // canStaticCast
+    static_assert(canStaticCast<bool,float>);
+    static_assert(canStaticCast<double,float>);
+    static_assert(canStaticCast<float,double>);
+    static_assert(!canStaticCast<std::string,bool>);
+    static_assert(!canStaticCast<long,std::string>);
+
+    // canDynamicCast
+    class CA { public: virtual ~CA(); };
+    class CB: public CA {};
+    static_assert(canDynamicCast<CA*,CA*>);
+    static_assert(canDynamicCast<CB*,CB*>);
+    static_assert(canDynamicCast<CA*,CB*>);
+    static_assert(canDynamicCast<CB*,CA*>);
+    static_assert(!canDynamicCast<bool*,void*>);
+
+    // canConstCast
+    static_assert(canConstCast<const long*,long*>);
+    static_assert(canConstCast<float*,const float*>);
+    static_assert(!canConstCast<float*,double*>);
+    static_assert(!canConstCast<float*,const double*>);
+    static_assert(!canConstCast<bool,int>);
+    static_assert(canConstCast<float&,const float&>);
+
+    // canReinterpretCast
+    static_assert(canReinterpretCast<unsigned long,void*>);
+    static_assert(canReinterpretCast<void**,signed long>);
+    static_assert(canReinterpretCast<void*,char**>);
+    static_assert(!canReinterpretCast<bool,int>);
+    static_assert(canReinterpretCast<float***,bool*>);
+    static_assert(canReinterpretCast<signed long,char[4]>);
+    static_assert(canReinterpretCast<double*,float[8]>);
+
+    // canAssign
+    static_assert(canAssign<int,int>);
+    static_assert(canAssign<int,bool>);
+    static_assert(canAssign<bool,int>);
+    static_assert(canAssign<float&,double&&>);
+    static_assert(canAssign<double&,float&&>);
+    static_assert(canAssign<short&&,char>);
+    static_assert(canAssign<std::string,char*>);
+    static_assert(!canAssign<float,std::string>);
+    static_assert(!canAssign<char,std::string>);
 }
